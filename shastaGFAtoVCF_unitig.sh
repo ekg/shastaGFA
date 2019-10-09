8f7909a40612ebaeb645f0b818ef923a3c4fe65c
Erik Garrison <erik.garrison@gmail.com>
2019-10-09 11:00:52 +0200

    compute unitigs one time and save to file

--- shastaGFAtoVCF_unitig.sh
+++ shastaGFAtoVCF_unitig.sh
@@ -20,18 +20,17 @@ base=$(basename $input .gfa)
 
 gimbricate -g $input -c $cov_min | vg view -F - >$base.blunt.gfa
 odgi build -g $base.blunt.gfa -s -o $base.og
-odgi unitig -i $base.og -l $unitig_min_begin -p $unitig_extend \
-    | minimap2 -t $minimap2_threads -c --cs $reference /dev/stdin \
+odgi unitig -i $base.og -l $unitig_min_begin -p $unitig_extend -f | pigz >$base.unitig.fq.gz
+minimap2 -t $minimap2_threads -c --cs $reference $base.unitig.fq.gz \
     | sort -k6,6 -k8,8n \
     | paftools.js call - >$base.paftools.vcf
 <$base.paftools.vcf awk 'function abs(v) {return v < 0 ? -v : v} /^V/ && abs(length($7) - length($8)) >100 { x=abs(length($7) - length($8)); print $2, $3-x, $4+x }' | tr ' ' '\t' | sort | uniq | bedtools merge -i - >$base.sv.bed
-odgi unitig -i $base.og -p $unitig_extend -f \
-    | minimap2 -t $minimap2_threads -c -a $reference /dev/stdin \
+minimap2 -t $minimap2_threads -c -a $reference $base.unitig.fq.gz \
     | samtools view -b - >$base.unitigs.raw.bam
 samtools sort $base.unitigs.raw.bam >$base.unitigs.bam
 rm -f $base.unitigs.raw.bam
 samtools index $base.unitigs.bam
-java -jar ~/jvarkit/dist/bamslicebed.jar -B $base.sv.bed $base.unitigs.bam | samtools view -b - >$base.unitigs.sv.slice.raw.bam
+java -jar ~/jvarkit/dist/bamslicebed.jar --bai -B $base.sv.bed $base.unitigs.bam | samtools view -b - >$base.unitigs.sv.slice.raw.bam
 samtools sort $base.unitigs.sv.slice.raw.bam >$base.unitigs.sv.slice.bam
 rm -f $base.unitigs.sv.slice.raw.bam
 samtools index $base.unitigs.sv.slice.bam
